9f9d102b59462f605a5d3d1acc8b3ef09aae2539
emacha <edmacha93@gmail.com>
2019-04-21 20:33:29 +0100

    Add comments

--- playlistify.py
+++ playlistify.py
@@ -7,27 +7,32 @@ from datetime import datetime
 
 
 def get_playlists(sp, user):
+    """Get all playlists by user."""
     items = sp.user_playlists(user)['items']
     playlists = {pl['name']: pl['id'] for pl in items}
     return playlists
 
 
 def get_n_last_liked(sp, n):
+    """Get n most recently liked songs by user."""
     items = sp.current_user_saved_tracks(n)['items']
     return items
 
 
 def sorted_id(items):
+    """Sort songs by when they were added."""
     items = list((it['track']['id'], it['added_at']) for it in items)
     items = sorted(items, key=lambda x: to_timestamp(x[1]), reverse=True)
     return [it[0] for it in items]
 
 
 def item_track_ids(items):
+    """Return track id from JSON."""
     return {it['track']['id'] for it in items}
 
 
 def to_timestamp(date):
+    """Convert string to UNIX timestamp."""
     ts = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ').timestamp()
     return ts
 
@@ -40,7 +45,7 @@ token = util.prompt_for_user_token(username, scope, client_id=credentials.client
                                    client_secret=credentials.client_secret, redirect_uri=credentials.redirect_uri)
 sp = spotipy.Spotify(auth=token)
 
-# Create playlist if it does not exist.
+# Create playlist if it does not exist
 pl_name = 'Recently liked'
 playlists = get_playlists(sp, username)
 if pl_name not in playlists:
@@ -49,10 +54,9 @@ if pl_name not in playlists:
 
 playlist_id = playlists[pl_name]
 
-# Get new/old songs and add/remove them.
+# Fetch songs and update playlist
 liked_songs = sorted_id(get_n_last_liked(sp, playlist_length))
 playlist_songs = item_track_ids(sp.user_playlist(username, playlist_id)['tracks']['items'])
-
 if not set(liked_songs) == playlist_songs:
     print('Updating!')
     sp.user_playlist_replace_tracks(username, playlist_id, liked_songs)
